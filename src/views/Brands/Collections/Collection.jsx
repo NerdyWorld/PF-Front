77a81fd229cdf6f6be3e4f648d19fd2633a63617
barbilusenra <baarbi.senra@gmail.com>
2023-09-11 02:11:58 -0300

    seeAll

--- src/views/Brands/Collections/Collection.jsx
+++ src/views/Brands/Collections/Collection.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import CarouselCollection from "./carouselCollection";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { brandsData } from "./brandsData";
 
 
 const Collection = () => {
+  const navigate = useNavigate()
   const { name } = useParams();
   const brand = brandsData[name];
   if(!brand) {
@@ -13,6 +14,7 @@ const Collection = () => {
 
 return(
   <div className="collection-container">
+    <p className='seeAll-collection' onClick={() => navigate('/seeAll/:name')}>See All</p>
     <h2 className="collection-brandName">{brand.brandName}</h2>
     <h3 className="collection-name">{brand.name}</h3>
     <CarouselCollection collections={brand.collections} />
